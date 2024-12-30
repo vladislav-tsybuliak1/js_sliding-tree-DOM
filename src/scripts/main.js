8eaa12adf4772dc48dc7684a7df82ac5cd9b6b72
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-30 17:42:34 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('li');
+
+for (const item of listItems) {
+  const textContent = item.childNodes[0];
+
+  if (textContent.nodeType === Node.TEXT_NODE) {
+    const span = document.createElement('span');
+
+    span.textContent = textContent.textContent.trim();
+    item.replaceChild(span, textContent);
+  }
+}
+
+document.querySelectorAll('span').forEach((span) => {
+  span.addEventListener('click', () => {
+    const parentLi = span.parentElement;
+    const nestedUl = parentLi.querySelector('ul');
+
+    if (nestedUl) {
+      if (nestedUl.style.display === 'none') {
+        nestedUl.style.display = 'block';
+      } else {
+        nestedUl.style.display = 'none';
+      }
+    }
+  });
+});
